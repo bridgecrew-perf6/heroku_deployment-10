0b4415b682529cd0d7b2f13925910e02d2bc9e25
tural <tetural@gmail.com>
2022-01-20 00:58:06 +0100

    it seems to be still sqllite in use, remove it to test with postgres db

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'tural'
 api = Api(app)
