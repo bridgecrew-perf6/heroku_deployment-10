9990130f3d607a26efd6fc712668e3509a95d6b6
tural <tetural@gmail.com>
2022-01-20 00:54:55 +0100

    Fixing "Can't load plugin: sqlalchemy.dialects:postgres" issue

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,8 +8,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'tural'
 api = Api(app)
